5c80d5acc3d99706f301036f552389362fe8a0b4
ak1ra24 <marug4580@gmail.com>
2019-12-01 03:23:57 +0900

    changed pr comment details

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -12,6 +12,10 @@ go test ./...
 echo "=====TEST END====="
 
 mkdir $BUILD_PATH/output-file
-echo "pr-test OK" >> $BUILD_PATH/output-file/comment.txt
+echo "TEST RESULT"
+echo "<details>" >> $BUILD_PATH/output-file/comment.txt
+echo "<summary>TEST RESULT</summary>" >> $BUILD_PATH/output-file/comment.txt
+go test ./... >> $BUILD_PATH/output-file/comment.txt
+echo "</details>" >> $BUILD_PATH/output-file/comment.txt
 ls -al $BUILD_PATH/output-file
 cat $BUILD_PATH/output-file/comment.txt
