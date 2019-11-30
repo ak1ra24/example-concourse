14236bf314f4b573e7261280fd01ff9f02d553d5
ak1ra24 <marug4580@gmail.com>
2019-11-30 19:05:13 +0900

    testcase02: what18

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -10,6 +10,6 @@ cd ${GOPATH}/src/github.com/ak1ra24/example-concourse
 
 go test ./...
 echo "=====TEST END====="
-echo "pr-test OK" >> $BUILD_PATH/github-comment/comment.txt
+echo "pr-test OK" >> $BUILD_PATH/pr/comment.txt
 ls -al $BUILD_PATH/
-cat $BUILD_PATH/github-comment/comment.txt
+cat $BUILD_PATH/pr/comment.txt
