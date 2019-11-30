86577205684e2a323a85b29cca56476700f0243c
ak1ra24 <marug4580@gmail.com>
2019-11-30 21:07:47 +0900

    push wiki

---
+++ ci/tasks/test-push-wiki.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+git config --global user.email "marug4580@gmail.com"
+git config --global user.name "ak1ra24"
+
+cd resource-example-wiki
+
+FILE="Wiki.md"
+ 
+if [ -e $FILE ]; then
+    echo "| date | time |\n" >> Home.md
+    echo "|---|---|\n" >> Home.md
+fi
+export DATE=$(date '+%Y/%m/%d')
+export TIME=$(date '+%R')
+echo "|$DATE|$TIME|" >> Home.md
+
+git add -A
+git commit -m "Update wiki $DATE $TIME"
+git push origin master
