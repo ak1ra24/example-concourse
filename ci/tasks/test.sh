f7c23a48ea664adef089affa913619ca2f05f3df
ak1ra24 <marug4580@gmail.com>
2019-11-30 17:39:40 +0900

    testcase02: ls -al

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -8,4 +8,5 @@ cd ${GOPATH}/src/github.com/ak1ra24/example-concourse
 echo "=====TEST START====="
 go test ./...
 echo "=====TEST END====="
-echo "pr-test OK" > pr/output.txt
+ls -al
+echo "pr-test OK" > output.txt
