b71f1bd010948fc7475a59dbdbc5fb669bf80a4c
ak1ra24 <marug4580@gmail.com>
2019-11-30 18:21:16 +0900

    testcase02: what7

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 echo "=====START====="
+pwd
+echo "=====TEST START====="
 mkdir -p ${GOPATH}/src/github.com/ak1ra24/example-concourse
 cp -r pr/* ${GOPATH}/src/github.com/ak1ra24/example-concourse
 cd ${GOPATH}/src/github.com/ak1ra24/example-concourse
 
-echo "=====TEST START====="
 go test ./...
 echo "=====TEST END====="
 ls -al /
