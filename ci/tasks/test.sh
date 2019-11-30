d20eba3e9b5270fe18debee9fe6e6a4583fec89d
ak1ra24 <marug4580@gmail.com>
2019-11-30 16:43:39 +0900

    change test.sh

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
+echo "=====START====="
 mkdir -p ${GOPATH}/src/github.com/ak1ra24/example-concourse
 cp -r pr/* ${GOPATH}/src/github.com/ak1ra24/example-concourse
 cd ${GOPATH}/src/github.com/ak1ra24/example-concourse
 
+echo "=====TEST START====="
 go test ./...
+echo "=====TEST END====="
