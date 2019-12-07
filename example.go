67880adde5731ac3a4b557266d357e511f6fea48
ak1ra24 <marug4580@gmail.com>
2019-12-08 08:25:27 +0900

    go test

--- example.go
+++ example.go
@@ -7,12 +7,6 @@ import (
 func example(code string) (int, error) {
 	if code == "hoge" {
 		return 1, nil
-	} else if code == "hogehoge" {
-		return 1, nil
-	} else if code == "hogehogehoge" {
-		return 1, nil
-	} else if code == "hogehogehogehoge" {
-		return 1, nil
 	}
 	return 0, errors.New("code must be hoge")
 }
